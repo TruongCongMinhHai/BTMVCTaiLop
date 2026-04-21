9cea2a2807ec6ebbe2c4c00436692aac0efa2456
Trương Công Minh Hải <conconghai@gmail.com>
2026-04-21 21:14:27 +0700

    Step 3: Tao lop ApplicationDbContext va cau hinh ConnectionString

---
+++ BTMVC/BTMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore; 
+using BTMVC.Models;            
+namespace BTMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
